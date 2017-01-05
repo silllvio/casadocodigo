07cd8dd00e9992d8c5bc322e17f9055d8bdad28f
silllvio <silllvio@hotmail.com>
2017-01-05 21:31:23 -0200

    SpringMVC I - Aula 05 - Final / RedirectAttributes e Chamada da controller  por método.

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.casadocodigo.loja.daos.ProdutoDao;
 import br.com.casadocodigo.loja.models.Produto;
@@ -28,12 +29,14 @@ public class ProdutosController {
 	}
 
 	@RequestMapping(method=RequestMethod.POST)
-	public String gravar(Produto produto) {
-		System.out.println(produto);
-		produtoDao.gravar(produto);
-		return "produtos/ok";
+	public ModelAndView gravar(Produto produto, RedirectAttributes redirectAttributes){
+	    System.out.println(produto);
+	    produtoDao.gravar(produto);
+	    redirectAttributes.addFlashAttribute("sucesso","Produto cadastrado com sucesso!");
+	    return new ModelAndView("redirect:produtos");
 	}
-
+	
+	
 	@RequestMapping("/form")
     public ModelAndView form(){
 
